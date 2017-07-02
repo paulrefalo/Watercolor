179d13a003d30287f9ffb28d3702354283705e36
Paul ReFalo <paul.refalo@gmail.com>
2017-07-02 09:20:51 -0700

    Added files and switched to tab VC
    Tab VC

--- Watercolors/AppDelegate.swift
+++ Watercolors/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        
+        coreDataStack?.autoSave(60)
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
