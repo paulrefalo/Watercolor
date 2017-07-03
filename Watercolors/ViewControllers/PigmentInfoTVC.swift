94244ef521ba79ba2dbef82faa17740021dc00ba
paulrefalo <paul.refalo@gmail.com>
2017-07-02 19:44:14 -0700

    working on Pigment

--- Watercolors/ViewControllers/PigmentInfoTVC.swift
+++ Watercolors/ViewControllers/PigmentInfoTVC.swift
@@ -14,54 +14,47 @@ class PigmentInfoTVC: UITableViewController {
     // MARK: - Properties
     
     var currentPigment: Pigment!
+    let estimatedCellHeight: CGFloat = 50
+
     
     // MARK: - IBOutlets
     
     @IBOutlet var PigmentImageView: UIImageView!
-    @IBOutlet var PigmentImageView1: UIImageView!
-    @IBOutlet var PigmentImageView2: UIImageView!
-    @IBOutlet var PigmentImageView3: UIImageView!
-    @IBOutlet var PigmentImageView4: UIImageView!
-    @IBOutlet var PigmentImageView5: UIImageView!
-    @IBOutlet var PigmentImageView6: UIImageView!
-    @IBOutlet var PigmentImageView7: UIImageView!
-    
-    @IBOutlet var PigmentNameLabel: UILabel!
+
     @IBOutlet var PigmentTypeLabel: UILabel!
     @IBOutlet var ChemicalNameLabel: UILabel!
     @IBOutlet var ChemicalStructureLabel: UILabel!
-    
     @IBOutlet var PropertiesLabel: UILabel!
-    
     @IBOutlet var PermanenceLabel: UILabel!
     @IBOutlet var ToxicityLabel: UILabel!
     @IBOutlet var HistoryLabel: UILabel!
     @IBOutlet var AltNamesLabel: UILabel!
-    
+
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PigmentNameLabel.text = currentPigment.pigment_name;
-        ChemicalNameLabel.text = currentPigment.chemical_name;
-        ChemicalStructureLabel.text = currentPigment.chemical_formula;
-        PropertiesLabel.text = currentPigment.properties;
-        PermanenceLabel.text = currentPigment.permanence;
-        ToxicityLabel.text = currentPigment.toxicity;
-        HistoryLabel.text = currentPigment.history;
-        AltNamesLabel.text = currentPigment.alternative_names;
+
+        ChemicalNameLabel.text = currentPigment.chemical_name
+        ChemicalStructureLabel.text = currentPigment.chemical_formula
+        PropertiesLabel.text = currentPigment.properties
+        PermanenceLabel.text = currentPigment.permanence
+        ToxicityLabel.text = currentPigment.toxicity
+        HistoryLabel.text = currentPigment.history
+        AltNamesLabel.text = currentPigment.alternative_names
         
         guard let pigmentImageName = currentPigment.image_name  else { return}
         
         PigmentImageView.image = UIImage(named: pigmentImageName)
-        PigmentImageView2.image = UIImage(named: pigmentImageName)
-        PigmentImageView2.image = UIImage(named: pigmentImageName)
-        PigmentImageView3.image = UIImage(named: pigmentImageName)
-        PigmentImageView4.image = UIImage(named: pigmentImageName)
-        PigmentImageView5.image = UIImage(named: pigmentImageName)
-        PigmentImageView6.image = UIImage(named: pigmentImageName)
-        PigmentImageView7.image = UIImage(named: pigmentImageName)
+        self.title = currentPigment.pigment_name
+
+
+        tableView.rowHeight = UITableViewAutomaticDimension;
+        tableView.estimatedRowHeight = estimatedCellHeight;
+
+        tableView.delegate = self
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,5 +62,24 @@ class PigmentInfoTVC: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 { return 0}
+
+        return 35
+    }
+
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+        // first section is fixed, others are dynamic
+        if (indexPath.section == 0) {
+            if indexPath.row == 0 {
+                return 50
+            } else {
+                return 80
+            }
+        }
+
+        return UITableViewAutomaticDimension
+    }
 }
